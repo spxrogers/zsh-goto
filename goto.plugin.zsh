8abcc3823d79dee98721234d73d2260d6c1317a8
arrdem <rmckenzie92@gmail.com>
2013-04-14 19:27:09 -0500

    rework & patch to support both a config variable and to resolve the user home dir better

--- goto.plugin.zsh
+++ goto.plugin.zsh
@@ -1,21 +1,23 @@
 # goto.zsh
 
-$GOTO_FILE=~/.labels.tsv
+function _gotofile {
+    echo $GOTO_FILE "$HOME/.labels.tsv" "$home/.labels.tsv" | awk "{print \$1}"
+}
+
+function _makeLabel {
+    printf "%s\t%s\n" $1 $2 >> `_gotofile`
+}
 
 function label {
-    function makeLabel {
-         printf "%s\t%s\n" $1 $2 >> $GOTO_FILE
-    }
-
     if [ $# -eq 0 ]
     then
         echo "Usage: $ label <name> [dir]"
         echo "    creates a label which goto can cd to"
-    if [ -d "$2" ]
+    elif [ -d "$2" ]
     then
-        makeLabel $1 $2
+        _makeLabel $1 $2
     else
-        makeLabel $1 $PWD
+        _makeLabel $1 $PWD
     fi
 }
 
@@ -26,22 +28,17 @@ function goto {
         echo "    jumps to a record set by label"
     elif [[ "$1" == "ls" ]]
     then
-        awk "{ print \$1 }" $GOTO_FILE
+        awk "{ print \$1 }" `_gotofile`
     else
-        if [ -d "$(awk "/^$1/ {print \$2;}" $GOTO_FILE)" ]
-        then
-            cd $(awk "/^$1/ {print \$2;}" $GOTO_FILE)
-        else
-            echo $1 " was not found!"
-        fi
+        cd $(awk "/^$1/ {print \$2;}" `_gotofile`)
     fi
 }
 
 function _goto {
-    for label in $(awk '{print $1}' $GOTO_FILE)
+    for label in $(awk '{print $1}' `_gotofile`)
     do
         compadd "$@" $label
     done
-}
+};
 
 compdef _goto goto
